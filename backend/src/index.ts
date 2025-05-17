de72a22f0a7809c5fae0ccb26e354bd33871ed38
Aditya Narayan Singh <richiaditya25@gmail.com>
2025-05-17 19:35:48 +0530

    signup route done and output folder removed from the generatoe client

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,35 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-app.post("/api/v1/signup", (c) => {
-  return c.text("Hello Hono!");
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json(); // as converting body to json is a asynchronous call
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  console.log(user);
+
+  const token = await sign({ id: user.id }, "secret");
+
+  return c.json({
+    jwt: token,
+  });
 });
 
 app.post("/api/v1/signin", (c) => {
