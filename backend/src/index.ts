b93aaa7c4dfc904d550336cf0fce8b57a9864def
Aditya Narayan Singh <richiaditya25@gmail.com>
2025-07-21 22:11:23 +0530

    1st middle ware done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
@@ -38,6 +38,28 @@ app.post("/api/v1/signup", async (c) => {
   return c.json({
     "your jwt token is": token,
   });
+});
+
+// MIDDLWWARE
+app.use("/api/v1/signin", async (c, next) => {
+  // before it hit any route, what I need to do? -> i need to get the header, take the email, verify the jwt token in the header, if its verified successfully then proceed or else not
+
+  const header = c.req.header("authorization") || "";
+  const token = header.split(" ")[1]; // as we will send token like Bearer "tokenndjncjdsncdiks"
+  console.log(token);
+  const response = await verify(token, c.env.JWT_SECRET);
+
+  if (response.id) {
+    //👉 I used response.id as we have signed it using the id
+    console.log("middleware working");
+    (c as any).set("userId", response.id);
+    await next();
+  } else {
+    c.status(403);
+    return c.json({
+      error: "unauthoried",
+    });
+  }
 });
 
 // SIGNIN route
